cfdebbe98a1ac9c9ce5e592ead29999b48ca6f02
Marcelo Machado <mjoelmachadojr@gmail.com>
2023-03-04 20:23:14 -0300

    template heritage & template block

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from recipes.views import home
+from . import views
 
 # domain/
 urlpatterns = [
-    path('', home),  # home
+    path('', views.home),  # home
+    path('recipes/<int:id>/', views.recipe),  # recipes
 ]
